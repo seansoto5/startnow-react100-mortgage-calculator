6e63e1ca59ece9295f2e474fa0ffd912be6b9cf6
seansoto5 <sean_soto125@yahoo.com>
2018-09-06 21:04:08 -0700

    Work on HTML elements

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,12 +1,78 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  // your Javascript goes here
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
   render() {
     return (
       <div className='container'>
-        {/* your JSX goes here */}
+        <div className='row'>
+          <div className='col-xs-12'>
+            <h1>
+              Mortgage Calculator
+            </h1><hr></hr>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-xs-3'>
+          <h4>
+            Loan Balance
+          </h4>
+          </div>
+          <div className='col-xs-6'>
+            <input className='balance' type='number'>
+            </input>
+          </div>
+          <div className='col-xs-3'></div>
+        </div>
+        <div className='row'>
+          <div className='col-xs-3'>
+          <h4>
+            Interest Rate (%)
+          </h4>
+          </div>
+          <div className='col-xs-6'>
+            <input className='rate' type='number'>
+            </input>
+          </div>
+          <div className='col-xs-3'></div>
+        </div>
+        <div className='row'>
+          <div className='col-xs-3'>
+          <h4>
+            Loan Term (Years)
+          </h4>
+          </div>
+          <div className='col-xs-6'>
+          <select className='term' placeholder="Select term">
+          </select>
+          </div>
+          <div className='col-xs-3'></div>
+        </div>
+        <div className='row'>
+          <div className='col-xs-12'>
+            <button className='submit' type='button' onClick=''>
+            Submit
+            </button>
+          </div>
+        </div>
+        <div className='row'>
+            <div className='col-xs-12'>
+              <p className='output' id='output'>
+              </p>
+            </div>
+        </div>
       </div>
     );
   }
 }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
